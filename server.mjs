f7e70185fe8d7afd8105b2b8c43e76e3f5d81706
Teerayoot injun <teerayoot5056@gmail.com>
2023-03-03 11:31:13 +0700

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,6 +1,21 @@
-import { createServer } from 'http';
-
-createServer((req, res) => {
-  res.write('Hello World!');
-  res.end();
-}).listen(process.env.PORT);
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+app.options('*', cors());
+const Pool = require('pg').Pool
+const db = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'leaflet_2020',
+  password: '1234',
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static(__dirname + '/www'));
+app.listen(3000, () => {
+  console.log('run on port 3000..')
+})
