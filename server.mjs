f95afd7b66522051c631c497189f00a521605404
Teerayoot injun <teerayoot5056@gmail.com>
2023-03-03 11:33:51 +0700

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,9 +1,4 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const app = express();
-app.use(cors());
-app.options('*', cors());
+import { createServer } from 'http';
 const Pool = require('pg').Pool
 const db = new Pool({
   user: 'postgres',
@@ -11,11 +6,34 @@ const db = new Pool({
   database: 'leaflet_2020',
   password: '1234',
 });
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(express.static(__dirname + '/www'));
-app.listen(3000, () => {
-  console.log('run on port 3000..')
-})
+
+createServer((req, res) => {
+  res.write('Hello World!');
+  res.end();
+}).listen(process.env.PORT);
+
+
+app.get('/api_province', (req, res) => {
+  sql = 'select  pv_th,count(*) ,ST_AsGeoJSON(b.geom) AS geojson \
+  from modis_join a  \
+  inner join province b on a.pv_th = b.pv_tn \
+  group by pv_th ,b.geom order by count desc limit 100 ';
+   let jsonFeatures = [];
+  db.query(sql).then((data) => {
+      var rows = data.rows;
+      rows.forEach((e) => {
+          let feature = {
+              type: 'Feature',
+              geometry: JSON.parse(e.geojson),
+              properties: e
+          };
+          jsonFeatures.push(feature);
+      });
+      let geoJson = {
+          type: 'FeatureCollection',
+          features: jsonFeatures
+      };
+      res.status(200).json(geoJson);
+  });
+
+});
